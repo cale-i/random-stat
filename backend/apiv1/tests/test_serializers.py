bdcd63ce9d7919c36b7656e71d8385e1b5d1046b
cale-i <calei078029@gmail.com>
2020-12-15 03:10:56 +0900

    add test for stats_data

--- backend/apiv1/tests/test_serializers.py
+++ backend/apiv1/tests/test_serializers.py
@@ -319,7 +319,6 @@ class TestCategorySerializer(TestCase):
 
         # バリデーションの結果を検証
         serializer.is_valid()
-        print(serializer.errors)
         self.assertEqual(serializer.is_valid(), True)
 
     def test_input_invalid_if_id_is_blank(self):
@@ -549,3 +548,63 @@ class TestTimeSerializer(TestCase):
             'date': time.date,
         }
         self.assertDictEqual(serializer.data, expected_data)
+
+
+class TestStatsDataSerializer(TestCase):
+    """
+    StatsDataSerializerのテストクラス
+    """
+    input_data = {
+        'id': '00200521_00200_001_area_00000_cat01_100_tab_020_time_2015000000',
+        'unit': '人',
+        'value': '127094745',
+        'area': TestAreaSerializer.input_data.copy(),
+        'time': TestTimeSerializer.input_data.copy(),
+        'stats_code': TestStatsCodeSerializer.input_data.copy(),
+        'category': [TestCategorySerializer.input_data.copy()],
+        'sub_category': [TestSubCategorySerializer.input_data.copy()],
+    }
+
+    def test_input_valid(self):
+        """入力データのバリデーション(OK)"""
+
+        # シリアライザを作成
+        input_data = self.input_data.copy()
+        serializer = StatsDataSerializer(data=input_data)
+
+        serializer.is_valid()
+        # バリデーションの結果を検証
+        self.assertEqual(serializer.is_valid(), True)
+
+    # def test_input_invalid_if_id_is_blank(self):
+    #     """入力データのバリデーション(NG: idが空文字)"""
+
+    #     # シリアライザを作成
+    #     input_data = self.input_data.copy()
+    #     input_data['id'] = ''
+
+    #     serializer = StatsDataSerializer(data=input_data)
+
+    #     # バリデーションの結果を検証
+    #     self.assertEqual(serializer.is_valid(), False)
+    #     self.assertCountEqual(serializer.errors.keys(), ['id'])
+    #     self.assertCountEqual(
+    #         [e.code for e in serializer.errors['id']],
+    #         ['blank'],
+    #     )
+
+    # def test_input_invalid_if_statistics_name_is_blank(self):
+    #     """入力データのバリデーション(NG: statistics_nameが空文字)"""
+
+    #     # シリアライザを作成
+    #     input_data = self.input_data.copy()
+    #     input_data['statistics_name'] = ''
+    #     serializer = StatsDataSerializer(data=input_data)
+
+    #     # バリデーションの結果を検証
+    #     self.assertEqual(serializer.is_valid(), False)
+    #     self.assertCountEqual(serializer.errors.keys(), ['statistics_name'])
+    #     self.assertCountEqual(
+    #         [e.code for e in serializer.errors['statistics_name']],
+    #         ['blank'],
+    #     )
