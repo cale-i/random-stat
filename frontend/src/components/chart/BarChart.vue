6f6635cda42461c4b1d739aa6af0a5b51d3a47da
cale-i <calei078029@gmail.com>
2020-12-18 10:40:10 +0900

    combine legends data

--- frontend/src/components/chart/BarChart.vue
+++ frontend/src/components/chart/BarChart.vue
@@ -1,1 +1,34 @@
-// 棒グラフのオプション等
+<template>
+    <div id="chart">
+        <b-container>
+            <chart
+                v-if="loaded"
+                :chartData="chartdata"
+                :options="options"
+            ></chart>
+        </b-container>
+    </div>
+</template>
+
+<script>
+    import chart from "@/services/chart.js"
+
+    export default {
+        name: 'BarChart',
+        components: {
+            chart
+        },
+        data: () => ({
+
+        }),
+        methods: {
+
+        },
+        async mounted () {
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
