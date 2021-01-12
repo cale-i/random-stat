8b75c0fb4203acc7fbab2352f19d6b91b85d73da
cale-i <calei078029@gmail.com>
2021-01-12 15:46:01 +0900

    remove comments

--- frontend/src/pages/HomePage.vue
+++ frontend/src/pages/HomePage.vue
@@ -1,19 +1,12 @@
 <template>
   <div id="home-page">
     <GlobalHeader />
-    <!-- <InputField/> -->
-    <!-- <OutputField/> -->
     <ChartContainer />
-    <b-container>
-      <!-- <chart :chartdata="chartData" :options="chartOptions"/> -->
-    </b-container>
   </div>
 </template>
 
 <script>
 import GlobalHeader from "@/components/GlobalHeader.vue";
-// import InputField from '@/components/InputField.vue'
-// import OutputField from '@/components/OutputField.vue'
 import ChartContainer from "@/components/ChartContainer.vue";
 
 // import { mapGetters } from 'vuex'
@@ -21,8 +14,6 @@ import ChartContainer from "@/components/ChartContainer.vue";
 export default {
   components: {
     GlobalHeader,
-    // InputField,
-    // OutputField,
     ChartContainer,
   },
   data: function() {
