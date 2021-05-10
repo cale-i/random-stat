28ca17aef6ef7698adcabea548949ae35fd96640
cale-i <calei078029@gmail.com>
2021-05-11 04:10:15 +0900

    Format

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,113 +1,104 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import api from '@/services/api'
+import Vue from "vue";
+import Vuex from "vuex";
+import api from "@/services/api";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 const itemModule = {
-    namespaced: true,
-    state: {
-        getURL:'list/books/',
-        postURL:'post/books/',
-        updateURL:'update/books/',
-        deleteURL:'delete/books/',
-    },
-    getters: {
-        getURL: state => state.getURL,
-        postURL: state => state.postURL,
-        updateURL: state => state.updateURL,
-        deleteURL: state => state.deleteURL,
-    },
-    mutations: {
-
-    },
-    actions: {
-        retlieve(context) {
-            return api({
-                method: 'get',
-                url: context.getters.getURL
-            })
-            .then(response => {
-                return response.data
-            })
-        },
-        create(context, payload) {
-            return api({
-                method: 'post',
-                url: context.getters.postURL,
-                data: {
-                    title: payload.title,
-                    price: payload.price
-                }
-            })
-            .then(response => {
-                return response.data
-                
-            })
-        },
-        put(context, payload) {
-            return api({
-                method: 'put',
-                url: context.getters.updateURL + payload.id + '/',
-                data: {
-                    id: payload.id,
-                    title: payload.title,
-                    price: payload.price,
-                    created_at: payload.created_at,
-
-                }
-            })
-        },
-        delete(context, payload) {
-            return api({
-                method: 'delete',
-                url:context.getters.deleteURL + payload.item.id + '/',
-            })
-            .then(response => {
-                return response
-            })
-        }
-    }
-}
+	namespaced: true,
+	state: {
+		getURL: "list/books/",
+		postURL: "post/books/",
+		updateURL: "update/books/",
+		deleteURL: "delete/books/",
+	},
+	getters: {
+		getURL: (state) => state.getURL,
+		postURL: (state) => state.postURL,
+		updateURL: (state) => state.updateURL,
+		deleteURL: (state) => state.deleteURL,
+	},
+	mutations: {},
+	actions: {
+		retlieve(context) {
+			return api({
+				method: "get",
+				url: context.getters.getURL,
+			}).then((response) => {
+				return response.data;
+			});
+		},
+		create(context, payload) {
+			return api({
+				method: "post",
+				url: context.getters.postURL,
+				data: {
+					title: payload.title,
+					price: payload.price,
+				},
+			}).then((response) => {
+				return response.data;
+			});
+		},
+		put(context, payload) {
+			return api({
+				method: "put",
+				url: context.getters.updateURL + payload.id + "/",
+				data: {
+					id: payload.id,
+					title: payload.title,
+					price: payload.price,
+					created_at: payload.created_at,
+				},
+			});
+		},
+		delete(context, payload) {
+			return api({
+				method: "delete",
+				url: context.getters.deleteURL + payload.item.id + "/",
+			}).then((response) => {
+				return response;
+			});
+		},
+	},
+};
 const chartModule = {
-    namespaced: true,
-    state: {
+	namespaced: true,
+	state: {
 		chartURL: "timeseries/",
-        chartURL: state => state.chartURL,
-        searchURL: state => state.searchURL,
-    },
-    mutations: {
-
-    },
-    actions: {
-        getChart(context) {
-            return api({
-                method: 'get',
-                url: context.getters.chartURL
-            })
-            .then(response => {
-                return response.data
-            })
-        },
-        searchChart(context, data) {
-            return api({
-                method: 'post',
-                url: context.getters.searchURL,
-                data,
-            })
-            .then(response => {
-                return response.data
-            })
-        },
-    }
-}
-
+		searchURL: "search/",
+	},
+	getters: {
+		chartURL: (state) => state.chartURL,
+		searchURL: (state) => state.searchURL,
+	},
+	mutations: {},
+	actions: {
+		getChart(context) {
+			return api({
+				method: "get",
+				url: context.getters.chartURL,
+			}).then((response) => {
+				return response.data;
+			});
+		},
+		searchChart(context, data) {
+			return api({
+				method: "post",
+				url: context.getters.searchURL,
+				data,
+			}).then((response) => {
+				return response.data;
+			});
+		},
+	},
+};
 
 const store = new Vuex.Store({
-    modules: {
-        item: itemModule,
-        chart: chartModule,
-    }
-})
+	modules: {
+		item: itemModule,
+		chart: chartModule,
+	},
+});
 
-export default store
+export default store;
