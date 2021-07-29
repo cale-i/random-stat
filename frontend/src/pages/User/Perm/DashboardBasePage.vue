13b420238ca947c0a6ff3dda942c788e83a33607
cale-i <calei078029@gmail.com>
2021-07-30 02:19:35 +0900

    Remove examples

--- frontend/src/pages/User/Perm/DashboardBasePage.vue
+++ frontend/src/pages/User/Perm/DashboardBasePage.vue
@@ -2,24 +2,10 @@
 	<div>
 		<GlobalHeader />
 		<GlobalMessage />
-		<GlobalSidebar />
 		<div>
 			<div class="row">
 				<div class="col-md-3"></div>
 				<div class="main col-md-9">
-					<b-row>
-						<b-col md="4">
-							<h1>ダッシュボード</h1>
-							<b-button variant="primary" @click="logout">logout</b-button>
-						</b-col>
-						<b-col md="4">
-							<router-link to="/dashboard">root</router-link>
-						</b-col>
-						<b-col md="4">
-							<router-link to="/dashboard/user">user</router-link>
-						</b-col>
-					</b-row>
-
 					<b-row>
 						<router-view />
 					</b-row>
@@ -32,13 +18,11 @@
 <script>
 import GlobalMessage from "@/components/GlobalMessage.vue";
 import GlobalHeader from "@/components/GlobalHeader.vue";
-import GlobalSidebar from "@/components/GlobalSidebar.vue";
 
 export default {
 	components: {
 		GlobalMessage,
 		GlobalHeader,
-		GlobalSidebar,
 	},
 	props: {},
 	data: () => ({
