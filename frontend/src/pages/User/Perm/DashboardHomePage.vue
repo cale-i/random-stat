bd52732e93cb1d9f7f462fe3beaee60689ba3007
cale-i <calei078029@gmail.com>
2021-07-30 02:20:14 +0900

    Change layout

--- frontend/src/pages/User/Perm/DashboardHomePage.vue
+++ frontend/src/pages/User/Perm/DashboardHomePage.vue
@@ -2,47 +2,33 @@
 	<div>
 		<b-row>
 			<b-col md="4">
-			<h1>ダッシュボード ほめ</h1>
-            aaa
+				<GlobalSidebar />
+			</b-col>
+
+			<b-col md="8">
+				<h1>ダッシュボード ほめ</h1>
+				aaa
 			</b-col>
 		</b-row>
-		<b-row>
-		</b-row>
+		<b-row> </b-row>
 	</div>
 </template>
 
 <script>
+import GlobalSidebar from "@/components/GlobalSidebar.vue";
+
 export default {
-	props: {
-
+	components: {
+		GlobalSidebar,
 	},
+	props: {},
 	data: () => ({
-		user: {
-
-		},
+		user: {},
 	}),
 	computed: {},
-	methods: {
-		logout () {
-			this.$store.dispatch('auth/logout')
-				.then(() => {
-					console.log('success logout')
-					// クエリ文字列に「next」がなければホーム画面へ
-					const next = this.$route.query.next || '/'
-					this.$router.replace(next)
-				})
-		},
-	},
-	watch: {
-	},
-	mounted() {
-
-	},
-	// updated() {
-	//   this.makeSelected();
-	// },
+	methods: {},
+	watch: {},
+	mounted() {},
 };
 </script>
-<style scoped>
-
-</style>
+<style scoped></style>
