e8db6f262c5a06257805b96cd19ddfb682d7a2cc
cale-i <calei078029@gmail.com>
2021-08-07 03:53:15 +0900

    Format page layout

--- frontend/src/pages/User/Perm/AccountPage.vue
+++ frontend/src/pages/User/Perm/AccountPage.vue
@@ -1,94 +1,119 @@
 <template>
 	<div class="container">
-		<h1>アカウント管理</h1>
 		<b-row>
-			<b-col></b-col>
-			<b-col> ユーザー名: {{ user.username }} </b-col>
-			<b-col>
-				<router-link to="set-username" class="btn btn-primary"
-					>ユーザー名変更</router-link
-				>
+			<b-col md="4">
+				<aside style="background-color:gray;">
+					<h2>アカウント</h2>
+					<h2>ログイン履歴</h2>
+				</aside>
 			</b-col>
-		</b-row>
-		<b-row>
-			<b-col></b-col>
-			<b-col> email: {{ user.email }} </b-col>
-			<b-col>
-				<router-link to="set-email" class="btn btn-primary"
-					>メールアドレス変更</router-link
-				>
-			</b-col>
-		</b-row>
-		<b-row>
-			<b-col></b-col>
-			<b-col>
-				<input type="text" :placeholder="user.first_name" />
-			</b-col>
-			<b-col>
-				<router-link to="set-email" class="btn btn-primary"
-					>firstname変更</router-link
-				>
-			</b-col>
-		</b-row>
-		<b-row>
-			<b-col></b-col>
-			<b-col>
-				<input type="text" :placeholder="user.last_name" />
-			</b-col>
-			<b-col>
-				<router-link to="set-email" class="btn btn-primary"
-					>lastname変更</router-link
-				>
-			</b-col>
-		</b-row>
-		<b-row>
-			<b-col></b-col>
-			<b-col> </b-col>
-			<b-col>
-				<router-link to="set-password" class="btn btn-primary"
-					>パスワード変更</router-link
-				>
-			</b-col>
-		</b-row>
+			<b-col md="8">
+				<h1>アカウント管理</h1>
+				<b-form-row class="username my-3">
+					<b-col md="3">
+						<label class="m-2" for="inputUsername">Username:</label>
+					</b-col>
+					<b-col md="6">
+						<b-form-input
+							id="inputUsername"
+							v-model="user.username"
+							type="text"
+							required
+							class=""
+						></b-form-input>
+					</b-col>
+					<b-col md="3">
+						<b-button size="md" variant="success" @click="changeUsername">
+							変更
+						</b-button>
+					</b-col>
+				</b-form-row>
 
-		<b-row>
-			<b-col md="8"></b-col>
-			<b-col>
-				<router-link to="delete-account" class="btn btn-warning"
-					>アカウント削除</router-link
-				>
+				<b-form-row class="email my-3">
+					<b-col md="3">
+						<label class="m-2" for="inputEmail">Email:</label>
+					</b-col>
+					<b-col md="6">
+						<b-form-input
+							id="inputEmail"
+							:placeholder="user.email"
+							type="email"
+							required
+							class=""
+							disabled
+						></b-form-input>
+					</b-col>
+					<b-col md="3">
+						<div v-b-modal.changeEmailModal class="btn btn-success">
+							変更
+						</div>
+					</b-col>
+				</b-form-row>
+
+				<b-row>
+					<b-col></b-col>
+					<b-col> </b-col>
+					<b-col>
+						<div v-b-modal.changePasswordModal class="btn btn-primary">
+							パスワード変更
+						</div>
+					</b-col>
+				</b-row>
+
+				<b-row>
+					<b-col md="8"></b-col>
+					<b-col>
+						<router-link to="delete-account" class="btn btn-warning"
+							>アカウント削除</router-link
+						>
+					</b-col>
+				</b-row>
 			</b-col>
 		</b-row>
-
-		<b-button @click="getUser()">GetUser</b-button>
+		<ChangeEmailModal />
+		<ChangePasswordModal />
 	</div>
 </template>
 
 <script>
+import ChangeEmailModal from "@/components/auth/ChangeEmailModal.vue";
+import ChangePasswordModal from "@/components/auth/ChangePasswordModal.vue";
+
+// import { mapGetters } from "vuex";
+
 export default {
+	components: {
+		ChangeEmailModal,
+		ChangePasswordModal,
+	},
 	props: {},
-	data: () => ({
-		user: {
-			username: "",
-			first_name: "",
-			last_name: "",
-			email: "",
+	data: () => ({}),
+	computed: {
+		user: function() {
+			return this.$store.state.auth;
 		},
-	}),
-	computed: {},
+	},
 	methods: {
-		async getUserData() {
-			const user = await this.$store.dispatch("auth/getUserData");
-			this.user = user;
+		changeUsername() {
+			console.log(this.user.username);
+
+			this.$store
+				.dispatch("auth/setUsername", {
+					username: this.user.username,
+				})
+				.then(() => {
+					console.log("username changed!");
+					this.$store.dispatch("message/setInfoMessage", {
+						message: "ユーザー名を変更しました",
+					});
+				});
 		},
 	},
 	watch: {},
 	mounted() {
-		this.getUserData();
+		// this.getUserData();
 	},
-	// updated() {
-	//   this.makeSelected();
-	// },
+	updated() {},
 };
 </script>
 <style scoped></style>
