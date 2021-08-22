179957558357befedb3830c1beb8da8c5ffc58d4
cale-i <calei078029@gmail.com>
2021-08-22 14:15:31 +0900

    Fix startup

--- backend/startup/db/startup_database.sh
+++ backend/startup/db/startup_database.sh
@@ -7,5 +7,3 @@ pip3 install -r requirements.txt
 
 echo "DATABASE_URL=postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:5432/${POSTGRES_NAME}" > .env
 python3 client.py insert_data 
-
-python3 client.py
