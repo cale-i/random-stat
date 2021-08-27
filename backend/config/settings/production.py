d390815a5a26209fe4fd4c144d62d5838abda92c
cale-i <calei078029@gmail.com>
2021-08-27 22:04:44 +0900

    Cleanup and relocate

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -8,16 +8,6 @@ env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 #####################
-# Security settings #
-#####################
-
-DEBUG = False
-
-SECRET_KEY = env('SECRET_KEY')
-
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
-
-#####################
 # Site Map settings #
 #####################
 
@@ -25,53 +15,65 @@ SITE_EMAIL = env('SITE_EMAIL')
 SITE_NAME = 'Random Stat'
 DOMAIN = 'random-stat.work'
 
-############
-# Database #
-############
+#####################
+# Security settings #
+#####################
+
+DEBUG = False
+SECRET_KEY = env('SECRET_KEY')
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+
+######################
+#   CORS settings    #
+######################
+
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:8080',
+    'http://127.0.0.1:8080',
+)
+
+#######################
+#  Database settings  #
+#######################
 
 DATABASES = {
     'default': env.db()
 }
-DATABASES['default']['ATOMIC_REQUESTS'] = False
-
-
-##################
-# REST Framework #
-##################
-
-##################
-#     MEDIA      #
-##################
 
 ################################
 #      S3 Bucket settings      #
 ################################
 
-
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
 AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
 
-#################
-# Email Backend #
-#################
+#######################
+#    Email Backend    #
+#######################
 
 EMAIL_HOST_USER = env('SITE_EMAIL')
 DEFAULT_FROM_EMAIL = env('SITE_EMAIL')
 EMAIL_HOST_PASSWORD = env('EMAIL_PASSWORD')
 
-#################
-#  Guest Login  #
-#################
+
+#################################
+#     Django REST framework     #
+#################################
+
+#######################
+#     Guest Login     #
+#######################
+
 GUEST_EMAIL = env('GUEST_EMAIL')
 GUEST_PASSWORD = env('GUEST_PASSWORD')
 
+#######################
+#   Cookie Settings   #
+#######################
 
-###################
-# Cookie Settings #
-###################
 JWT_COOKIE = {
     'SAMESITE': 'Strict',
     # 'SECURE': True, 本番時 True
