6460379ef995ad1dd1cbc803ebf34adb4619efe1
cale-i <calei078029@gmail.com>
2021-09-02 23:47:49 +0900

    Add disconnect route

--- backend/social/urls.py
+++ backend/social/urls.py
@@ -1,6 +1,11 @@
 from django.urls import re_path, path
+from django.conf import settings
+
+from social_core.utils import setting_name
 
 from social import views
+
+extra = getattr(settings, setting_name('TRAILING_SLASH'), True) and '/' or ''
 
 urlpatterns = [
     re_path(
@@ -8,5 +13,9 @@ urlpatterns = [
         views.CustomProviderAuthView.as_view(),
         name="provider-auth",
     ),
+
+    # disconnection
+    re_path(r'^disconnect/(?P<backend>[^/]+){0}$'.format(extra), views.DisconnectView.as_view(),
+            name='disconnect'),
     path('', views.UserSocialAuthAPIView.as_view())
 ]
