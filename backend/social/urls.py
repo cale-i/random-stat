787a896f90396e495dcedb9254bbf536715576c0
cale-i <calei078029@gmail.com>
2021-09-05 14:08:32 +0900

    Add connect endpoint

--- backend/social/urls.py
+++ backend/social/urls.py
@@ -8,6 +8,7 @@ from social import views
 extra = getattr(settings, setting_name('TRAILING_SLASH'), True) and '/' or ''
 
 urlpatterns = [
+    # ソーシャルアカウントを使ってログイン
     re_path(
         r"^o/(?P<provider>\S+)/$",
         views.CustomProviderAuthView.as_view(),
@@ -17,5 +18,13 @@ urlpatterns = [
     # disconnection
     re_path(r'^disconnect/(?P<backend>[^/]+){0}$'.format(extra), views.DisconnectView.as_view(),
             name='disconnect'),
-    path('', views.UserSocialAuthAPIView.as_view())
+
+    # ログイン後､追加のアカウント連携
+    re_path(
+        r"^connect/(?P<provider>\S+)/$",
+        views.ConnectView.as_view(),
+        name="provider-auth",
+    ),
+
+    path('services/', views.UserSocialAuthAPIView.as_view())
 ]
