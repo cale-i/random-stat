711a7ac1fc4732c8c8e9d622564c2582f2f37555
cale-i <calei078029@gmail.com>
2021-09-06 03:22:26 +0900

    Create LoginModal

--- frontend/src/components/account/auth/LoginModal.vue
+++ frontend/src/components/account/auth/LoginModal.vue
@@ -1,129 +1,137 @@
 <template>
-	<b-modal
-		id="loginModal"
-		title="Login"
-		header-bg-variant="dark"
-		header-text-variant="light"
-		body-bg-variant="light"
-		body-text-variant="dark"
-		footer-bg-variant="light"
-		footer-text-variant="dark"
-		centered
-		no-stacking
-		size="md"
-	>
-		<template #modal-header="{}">
-			<b-col md="4">
-				<div v-b-modal.signUpModal class="btn btn-sm btn-warning">
-					Sign Up
+	<div>
+		<b-modal
+			id="loginModal"
+			header-bg-variant="dark"
+			header-text-variant="light"
+			body-bg-variant="light"
+			body-text-variant="dark"
+			footer-bg-variant="light"
+			footer-text-variant="dark"
+			centered
+			no-stacking
+			size="md"
+		>
+			<template #modal-header="{}">
+				<div class="header d-flex flex-grow-1">
+					<b-icon icon="bar-chart-line" aria-hidden="true"></b-icon>
+					<div class="ml-2">Random Stat</div>
 				</div>
-			</b-col>
-			<b-col md="4">
-				<h4 class="text-center">Login</h4>
-			</b-col>
-			<b-col md="4"> </b-col>
-		</template>
-		<template #default="{}">
-			<div class="card-body">
-				<b-form @submit.prevent="submitLogin">
-					<b-form-group
-						id="inputGroupEmail"
-						label="Email:"
-						label-cols-md="3"
-						label-align-md="right"
-						label-for="inputEmail"
-						class="my-4"
-					>
-						<b-form-input
-							id="inputEmail"
-							v-model="form.email"
-							type="email"
-							placeholder="example@example.com"
-							required
-							autofocus
-						></b-form-input>
-					</b-form-group>
+			</template>
+			<template #default="{}">
+				<div class="p-0">
+					<h4 class="text-center my-2 font-weight-bold">ログイン</h4>
 
-					<b-form-group
-						id="inputGroupPassword"
-						label="Password"
-						label-cols-md="3"
-						label-align-md="right"
-						label-for="inputPassword"
-						class="my-4"
-					>
-						<b-form-input
-							id="inputPassword"
-							v-model="form.password"
-							type="password"
-							placeholder="password"
-							required
-							autocomplete="true"
-						></b-form-input>
-					</b-form-group>
+					<hr />
 
-					<div
-						class="d-flex align-items-center justify-content-between mt-4 mb-0"
-					>
-						<div
-							v-b-modal.resetPasswordModal
-							class="btn btn-sm btn-link text-black-50"
-						>
-							パスワードをお忘れの場合
-						</div>
-						<b-button size="md" variant="success" type="submit">
-							Login
-						</b-button>
+					<div class="button google-login" @click="googleLogin">
+						<b-icon icon="google" aria-hidden="true"></b-icon>
+						<div>Google</div>
 					</div>
-				</b-form>
-			</div>
-		</template>
-		<template #modal-footer="{}">
-			<b-col md="3" v-b-modal.guestLoginModal class="btn btn-sm btn-info">
-				Guest Login
-			</b-col>
-			<b-col></b-col>
-			<b-col md="4" class="text-muted small">&copy; Random Stat 2021</b-col>
-		</template>
-	</b-modal>
+					<div class="button github-login">
+						<b-icon icon="github" aria-hidden="true"></b-icon>
+						<div>GitHub</div>
+					</div>
+					<div class="button twitter-login">
+						<b-icon icon="twitter" aria-hidden="true"></b-icon>
+						<div>Twitter</div>
+					</div>
+
+					<hr />
+
+					<div v-b-modal.emailLoginModal class="button email-login">
+						<b-icon icon="envelope-fill" aria-hidden="true"></b-icon>
+						<div>メールアドレス</div>
+					</div>
+					<div class="button guest-login" @click="guestLogin">
+						<b-icon icon="person-circle" aria-hidden="true"></b-icon>
+						<div>ゲストログイン</div>
+					</div>
+					<hr />
+
+					<div v-b-modal.signUpModal class="button sign-up">
+						<b-icon icon="pencil-square" aria-hidden="true"></b-icon>
+						<div>アカウント登録</div>
+					</div>
+				</div>
+			</template>
+			<template #modal-footer="{}">
+				<div class="d-flex justify-content-center flex-grow-1 text-muted small">
+					Random Stat 2021
+				</div>
+			</template>
+		</b-modal>
+		<EmailLoginModal />
+	</div>
 </template>
 
 <script>
+import EmailLoginModal from "@/components/account/auth/EmailLoginModal.vue";
 export default {
-	components: {},
+	components: {
+		EmailLoginModal,
+	},
 	props: {},
-	data: () => ({
-		form: {
-			email: "",
-			password: "",
-		},
-	}),
+	data: () => ({}),
 	computed: {},
 	methods: {
-		submitLogin() {
-			// ログイン
-			this.$store
-				.dispatch("auth/login", {
-					email: this.form.email,
-					password: this.form.password,
-				})
-				.then(() => {
-					console.log("success");
-					this.$store.dispatch("message/setInfoMessage", {
-						message: "ログインしました。",
-					});
-					// クエリ文字列に「next」がなければダッシュボード画面へ
-					const next = this.$route.query.next || "/dashboard";
-					console.log("next:", next);
-					this.$router.replace(next);
-				})
-				.catch(() => {
-					// ログイン失敗
-				});
+		googleLogin() {
+			this.$store.dispatch("socialAuth/googleLogin");
+		},
+		guestLogin() {
+			this.$store.dispatch("auth/guestLogin").then(() => {
+				// クエリ文字列に「next」がなければダッシュボード画面へ
+				const next = this.$route.query.next || "/dashboard";
+				this.$router.replace(next);
+			});
 		},
 	},
 	watch: {},
 	mounted() {},
 };
 </script>
-<style scoped></style>
+<style scoped>
+.header {
+	align-items: center;
+	height: 1.6rem;
+	font-size: 1.6rem;
+	color: white;
+	justify-content: center;
+	align-items: center;
+	user-select: none;
+}
+.button {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 3rem;
+	font-size: 1.2rem;
+	color: white;
+	margin: 0.5rem 2rem;
+	user-select: none;
+}
+.button:hover {
+	opacity: 0.8;
+}
+.button > svg {
+	margin-right: 0.5rem;
+}
+.google-login {
+	background: #db4437;
+}
+.github-login {
+	background: #333;
+}
+.twitter-login {
+	background: #1da1f2;
+}
+.email-login {
+	background: #00a040;
+}
+.guest-login {
+	background: gray;
+}
+.sign-up {
+	background: #bd3f4c;
+}
+</style>
