027f47179c0e7c766bda019f779377e151ad3599
cale-i <calei078029@gmail.com>
2021-09-19 02:00:08 +0900

    Add endpoint

--- backend/apiv1/urls.py
+++ backend/apiv1/urls.py
@@ -5,6 +5,7 @@ from apiv1 import views
 app_name = 'apiv1'
 urlpatterns = [
     path('timeseries/', views.TimeSeriesAPIView.as_view()),
+    path('timeseries/history/', views.StatHistoryView.as_view()),
     path('search/', views.TimeSeriesAPIView.as_view()),
     path('search/statscode/', views.StatsCodeAPIView.as_view()),
 
