0c8d4a418269bd98f10581684ebc06289b5ff433
cale-i <calei078029@gmail.com>
2021-09-26 07:21:19 +0900

    Create footer

--- frontend/src/pages/User/Perm/AccountPage.vue
+++ frontend/src/pages/User/Perm/AccountPage.vue
@@ -1,50 +1,55 @@
 <template>
-	<b-container>
-		<GlobalHeader />
-		<GlobalMessage />
-		<div class="container">
-			<b-row align-v="baseline">
-				<b-col md="3" class="bg-white mt-5">
-					<aside class="mt-5 mb-3 sidebar">
-						<b-row>
-							<router-link to="settings" class="sidebar-item">
-								アカウント管理
-							</router-link>
-						</b-row>
+	<div>
+		<b-container>
+			<GlobalHeader />
+			<GlobalMessage />
+			<div class="container">
+				<b-row align-v="baseline">
+					<b-col md="3" class="bg-white mt-5">
+						<aside class="mt-5 mb-3 sidebar">
+							<b-row>
+								<router-link to="settings" class="sidebar-item">
+									アカウント管理
+								</router-link>
+							</b-row>
 
-						<hr />
+							<hr />
 
-						<b-row>
-							<router-link to="login-record" class="sidebar-item">
-								ログイン履歴
-							</router-link>
-						</b-row>
+							<b-row>
+								<router-link to="login-record" class="sidebar-item">
+									ログイン履歴
+								</router-link>
+							</b-row>
 
-						<hr />
+							<hr />
 
-						<b-row>
-							<router-link to="social" class="sidebar-item">
-								アカウント連携
-							</router-link>
-						</b-row>
-					</aside>
-				</b-col>
-				<b-col></b-col>
-				<b-col md="8" class="bg-white">
-					<router-view />
-				</b-col>
-			</b-row>
-		</div>
-	</b-container>
+							<b-row>
+								<router-link to="social" class="sidebar-item">
+									アカウント連携
+								</router-link>
+							</b-row>
+						</aside>
+					</b-col>
+					<b-col></b-col>
+					<b-col md="8" class="bg-white">
+						<router-view />
+					</b-col>
+				</b-row>
+			</div>
+		</b-container>
+		<GlobalFooter />
+	</div>
 </template>
 
 <script>
 import GlobalMessage from "@/components/GlobalMessage.vue";
 import GlobalHeader from "@/components/GlobalHeader.vue";
+import GlobalFooter from "@/components/GlobalFooter.vue";
 export default {
 	components: {
 		GlobalMessage,
 		GlobalHeader,
+		GlobalFooter,
 	},
 	props: {},
 	data: () => ({}),
