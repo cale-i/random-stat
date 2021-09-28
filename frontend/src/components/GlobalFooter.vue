74437fe4ca4be7a88c86e50e351fece6c86a0a92
cale-i <calei078029@gmail.com>
2021-09-29 04:13:18 +0900

    Modify link url

--- frontend/src/components/GlobalFooter.vue
+++ frontend/src/components/GlobalFooter.vue
@@ -1,32 +1,37 @@
 <template>
-	<footer id="footer" class="footer">
-		<b-navbar class="d-block" type="dark" variant="dark" fixed="bottom">
-			<b-row>
-				<b-col class="footer-right"> </b-col>
-				<b-col class="footer-middle">
-					<div class="footer-copyright">
-						<b-icon
-							icon="bar-chart-line"
-							aria-hidden="true"
-							class="mr-2"
-						></b-icon>
-						<span class="rs-green">R</span>andom
-						<span class="rs-red">S</span>tat
-					</div>
-				</b-col>
-				<b-col class="footer-left">
-					<b-row class="pr-5">
-						<b-col>
-							<router-link to="/account/">このサイトについて</router-link>
-						</b-col>
-						<a href="https://github.com/cale-i/random-stat" class="text-white">
-							<b-icon icon="github"></b-icon>
-						</a>
-					</b-row>
-				</b-col>
-			</b-row>
-		</b-navbar>
-	</footer>
+	<div>
+		<footer id="footer" class="footer">
+			<b-navbar class="d-block" type="dark" variant="dark" fixed="bottom">
+				<b-row>
+					<b-col class="footer-right"> </b-col>
+					<b-col class="footer-middle">
+						<div class="footer-copyright">
+							<b-icon
+								icon="bar-chart-line"
+								aria-hidden="true"
+								class="mr-2"
+							></b-icon>
+							<span class="rs-green">R</span>andom
+							<span class="rs-red">S</span>tat
+						</div>
+					</b-col>
+					<b-col class="footer-left">
+						<b-row class="pr-5">
+							<b-col>
+								<router-link to="/about">このサイトについて</router-link>
+							</b-col>
+							<a
+								href="https://github.com/cale-i/random-stat"
+								class="text-white"
+							>
+								<b-icon icon="github"></b-icon>
+							</a>
+						</b-row>
+					</b-col>
+				</b-row>
+			</b-navbar>
+		</footer>
+	</div>
 </template>
 <style scoped>
 .footer {
