a78990aed09a62e905ac5d12ec172d5f9b4deef3
cale-i <calei078029@gmail.com>
2021-10-27 01:43:49 +0900

    Add factory-boy settings for area

--- backend/apiv1/tests/factories/estat.py
+++ backend/apiv1/tests/factories/estat.py
@@ -34,3 +34,16 @@ class StatsCodeFactory(factory.django.DjangoModelFactory):
     stat_name = factory.SubFactory(StatNameFactory)
     gov_org = factory.SubFactory(GovOrgFactory)
     title = factory.SubFactory(TitleFactory)
+
+
+class AreaFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Area
+
+    @factory.post_generation
+    def stats_code(self, create, extracted, **kwargs):
+        if not create:
+            return
+        if extracted:
+            for stat_code in extracted:
+                self.stats_code.add(stat_code)
