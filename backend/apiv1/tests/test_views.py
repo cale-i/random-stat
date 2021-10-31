04361ceb9436f0a33fcf2b1b06359ced81829c60
cale-i <calei078029@gmail.com>
2021-10-31 18:13:08 +0900

    Add test for CategoryListView

--- backend/apiv1/tests/test_views.py
+++ backend/apiv1/tests/test_views.py
@@ -1,6 +1,7 @@
 from estat.models import (
     StatsCode,
     Area,
+    Category,
 )
 from rest_framework.test import APITestCase
 
@@ -88,3 +89,50 @@ class TestAreaListView(APITestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, expected_json_list)
 
+
+class TestCategoryListView(APITestCase):
+    # """CategoryListViewのテストクラス"""
+    fixtures = ['apiv1/fixtures/test_category.json']
+    TARGET_URL = '/api/v1/timeseries/category/'
+
+    def test_get_success(self):
+        """モデル取得APIへのGETリクエスト(正常系)"""
+        params = {'stats_code': '0003410379'}
+
+        response = self.client.get(
+            self.TARGET_URL,
+            params,
+            format='json')
+
+        # レスポンスの内容を検証
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_bad_request_if_no_params(self):
+        """
+        モデル取得APIへのGETリクエスト(異常系)
+        stats_codeを指定していない場合
+        """
+        response = self.client.get(
+            self.TARGET_URL,
+            format='json')
+
+        # レスポンスの内容を検証
+        self.assertEqual(response.status_code, 400)
+
+    def test_get_bad_request_if_invalid_params(self):
+        """
+        モデル取得APIへのGETリクエスト(異常系)
+        存在しないstats_codeを指定した場合
+        """
+        params = {'stats_code': '1234567890'}
+
+        response = self.client.get(
+            self.TARGET_URL,
+            params,
+            format='json')
+
+        expected_json_list = []
+
+        # レスポンスの内容を検証
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, expected_json_list)
