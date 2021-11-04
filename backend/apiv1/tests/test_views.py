2bebf019b500311f29e3a7dc3f9ae392d98870d8
cale-i <calei078029@gmail.com>
2021-11-04 16:13:10 +0900

    Add import

--- backend/apiv1/tests/test_views.py
+++ backend/apiv1/tests/test_views.py
@@ -1,9 +1,11 @@
+from django.contrib.auth import get_user_model
 from estat.models import (
     StatsCode,
     Area,
     Category,
 )
 from rest_framework.test import APITestCase
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 class TestStatsCodeListView(APITestCase):
